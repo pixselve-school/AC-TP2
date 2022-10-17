9229b42b1469e898f44766e3a7a87497c38fb1d2
Pixselve <38262536+Pixselve@users.noreply.github.com>
2022-10-17 15:56:13 +0200

    feat

--- p2/AC_tp2_part2_donnees/FFT_2D.java
+++ p2/AC_tp2_part2_donnees/FFT_2D.java
@@ -45,7 +45,22 @@ public class FFT_2D {
   // FI contient la TDF de I
   // Dans FI on met à zéros tous les coefficients correspondant à des fréquences inférieures à k
   public static void compression(CpxImg FI, int k) {
-    // A COMPLETER
+    // Cette
+    //fonction doit annuler tous les coefficients de FI (FI sera la transformée de Fourier
+    //de l’image I à compresser) qui sont en dehors du carré de coté 2k centré sur le
+    //centre du tableau (i.e, n/2, n/2).
+
+    int n = FI.taille();
+    int centre = n / 2;
+    for (int i = 0; i < n; i++) {
+      for (int j = 0; j < n; j++) {
+        if (centre - k > i || i > centre + k || centre - k > j || j > centre + k) {
+          FI.set_p_reel(i, j, 0);
+          FI.set_p_imag(i, j, 0);
+        }
+      }
+    }
+
   }
 
   // compression par seuillage des coefficients faibles
@@ -53,13 +68,26 @@ public class FFT_2D {
   // Dans FI on met à zéros tous les coefficients dont le module est inférieur à seuil
   // on renvoie le nombre de coefficients conservés
   public static int compression_seuil(CpxImg FI, double seuil) {
-    //A COMPLETER
-    return 0;
+    int n = FI.taille();
+    int nb_coeff = 0;
+    for (int i = 0; i < n; i++) {
+      for (int j = 0; j < n; j++) {
+        double reel = FI.get_p_reel(i, j);
+        double imag = FI.get_p_imag(i, j);
+        double module = Math.sqrt(reel * reel + imag * imag);
+        if (module < seuil) {
+          FI.set_p_reel(i, j, 0);
+          FI.set_p_imag(i, j, 0);
+        } else {
+          nb_coeff++;
+        }
+      }
+    }
+    return nb_coeff;
   }
 
 
-  public static void main(String[] args) {
-
+  public static void exercice1() {
 //    try {
     // Pour n = 16
     // vecteur constant a = (a, . . . , a). Testez différentes valeurs de a ? R.
@@ -142,4 +170,157 @@ public class FFT_2D {
     }
   }
 
+  public static void exercice4_1() {
+    try {
+
+      List<String> images = new ArrayList<>();
+      images.add("fingerprint");
+      images.add("mire2");
+      images.add("barbara_512");
+      images.add("tigre_512");
+      images.add("lena_512");
+      images.add("mire3");
+      images.add("mire1");
+      for (String image : images) {
+        //      Exemple, lecture
+        BytePixmap BP = new BytePixmap("p2/AC_tp2_part2_donnees/" + image + ".pgm");
+        CpxImg I = new CpxImg(BP);
+
+        // Do the FFT
+        CpxImg FI = FFT(I);
+        FI.set_p_reel(FI.taille() / 2, FI.taille() / 2, 0);
+        FI.set_p_imag(FI.taille() / 2, FI.taille() / 2, 0);
+        //Exemple, écriture
+        BP = FI.convert_to_BytePixmap();
+        BP.write("p2/results/exercice4/" + image + "_fft.pgm");
+
+
+        // Do the iFFT
+        CpxImg IFI = FFT_inverse(FI);
+        BP = IFI.convert_to_BytePixmap();
+        BP.write("p2/results/exercice4/" + image + "_ifft.pgm");
+      }
+
+
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
+
+  public static void exercice4_2() {
+    try {
+
+      List<String> images = new ArrayList<>();
+      images.add("fingerprint");
+      images.add("mire2");
+      images.add("barbara_512");
+      images.add("tigre_512");
+      images.add("lena_512");
+      images.add("mire3");
+      images.add("mire1");
+      for (String image : images) {
+        //      Exemple, lecture
+        BytePixmap BP = new BytePixmap("p2/AC_tp2_part2_donnees/" + image + ".pgm");
+        CpxImg I = new CpxImg(BP);
+
+        // Do the FFT
+        CpxImg FI = FFT(I);
+        FI.set_p_reel(FI.taille() / 2 + 1, FI.taille() / 2, 0);
+        FI.set_p_imag(FI.taille() / 2 + 1, FI.taille() / 2, 0);
+        //Exemple, écriture
+        BP = FI.convert_to_BytePixmap();
+        BP.write("p2/results/exercice4_2/" + image + "_fft.pgm");
+
+
+        // Do the iFFT
+        CpxImg IFI = FFT_inverse(FI);
+        BP = IFI.convert_to_BytePixmap();
+        BP.write("p2/results/exercice4_2/" + image + "_ifft.pgm");
+      }
+
+
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
+
+  public static void exercice5() {
+    try {
+      List<String> images = new ArrayList<>();
+      images.add("fingerprint");
+      images.add("mire2");
+      images.add("barbara_512");
+      images.add("tigre_512");
+      images.add("lena_512");
+      images.add("mire3");
+      images.add("mire1");
+      for (int k = 1; k < 100; k *= 2) {
+        for (String image : images) {
+          //      Exemple, lecture
+          BytePixmap BP = new BytePixmap("p2/AC_tp2_part2_donnees/" + image + ".pgm");
+          CpxImg I = new CpxImg(BP);
+
+
+          // Do the FFT
+          CpxImg FI = FFT(I);
+          compression(FI, k);
+          // Do the iFFT
+          CpxImg IFI = FFT_inverse(FI);
+          BP = IFI.convert_to_BytePixmap();
+          //Exemple, écriture
+          BP.write("p2/results/exercice5/" + image + "_compressed_k=" + k + ".pgm");
+
+
+        }
+      }
+
+
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
+
+  public static void exercice6() {
+    try {
+      List<String> images = new ArrayList<>();
+      images.add("fingerprint");
+      images.add("mire2");
+      images.add("barbara_512");
+      images.add("tigre_512");
+      images.add("lena_512");
+      images.add("mire3");
+      images.add("mire1");
+      for (int k = 1; k < 100; k *= 2) {
+        for (String image : images) {
+          //      Exemple, lecture
+          BytePixmap BP = new BytePixmap("p2/AC_tp2_part2_donnees/" + image + ".pgm");
+          CpxImg I = new CpxImg(BP);
+
+
+          // Do the FFT
+          CpxImg FI = FFT(I);
+          compression_seuil(FI, k);
+          // Do the iFFT
+          CpxImg IFI = FFT_inverse(FI);
+          BP = IFI.convert_to_BytePixmap();
+          //Exemple, écriture
+          BP.write("p2/results/exercice6/" + image + "_compressed_k=" + k + ".pgm");
+
+
+        }
+      }
+
+
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
+
+
+  public static void main(String[] args) {
+    exercice5();
+    exercice6();
+  }
+
 }
+
